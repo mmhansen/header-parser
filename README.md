89f9571035cfff853302e4ba27559174f6703b80
Michael Hansen <hansen.mmb@gmail.com>
2016-09-26 15:42:46 -0400

    V 1.0.0 gets info from headers displays at all routes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Header Parsing
+Header Parsing  
 
-
-1. User Story: I can get the IP address, language and operating system for my browser.
+User Story:  
+1. I can get the IP address, language and operating system for my browser.
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var path = require('path');
+var app = express();
 app.use(express.static('./public'))
+
+app.get('*', function(req, res){
+  var head = req.headers;
+  var response = {
+    "ip": head['host'],
+    "language": head['accept-language'].split(",").slice(0,1)[0],
+    "operating system": head['user-agent'].split(" ").slice(2,3)[0]
+  }
+  res.json(response);
+})
+
+
 
 var server = app.listen(process.env.PORT || 3000, function(){
   console.log('App listening on port 3000')
