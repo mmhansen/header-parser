3e5da9ae1f32b225d80adb6ab6651a7c86591781
Michael Hansen <hansen.mmb@gmail.com>
2016-09-26 16:05:24 -0400

    fixed another typo, moved os to variable to be more clear about format

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ app.use(express.static('./public'))
 
 app.get('*', function(req, res){
   var head = req.headers;
+	var user = head['user-agent'];
+	var os = user.substring(user.indexOf("(")+1, user.indexOf(")"));
 
   var response = {
     "ip": head['x-forwarded-for'] || req.connection.remoteAddress,
     "language": head['accept-language'].split(",").slice(0,1)[0],
-    "operating system": head['user-agent']substring(head['user-agent'].indexOf('(')+1, head['user-agent'].indexOf(')'))
+    "operating system": os
   }
   res.json(response);
 })
